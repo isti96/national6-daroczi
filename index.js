efae05522d18df45bf651acef2f6681e737b1b1c
Johann Cruyff <isti.daroczi@gmail.com>
2021-03-01 17:39:37 +0200

    clean start

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("JavaScript - Async Programming");
+
+
+
+document.addEventListener("click", onClick);
+
+let counter = 0;
+
+function onClick() {
+  console.log("just a click in the app");
+  counter++;
+  console.log("counter:", counter);
+}
+console.log("counter:", counter);
+
+
+setTimeout(function () {
+  console.log("1");
+}, 2000);
+console.log("2");
+
+
+setTimeout(setTimeoutFunction, 2000);
+
+function setTimeoutFunction() {
+  console.log("3");
+}
+
+console.log("First");
+setTimeout(function () {
+  console.log("Second");
+}, 0);
+setTimeout(function () {
+  console.log("Third");
+}, 0);
+console.log("Fourth");
+setTimeout(function () {
+  console.log("Fifth");
+}, 1);
+
+
+setInterval(function () {
+  console.log("ping");
+}, 1000);
