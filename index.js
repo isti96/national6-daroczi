12f726de003ac8ba183bc4c23f892dec3ebe8638
Johann Cruyff <isti.daroczi@gmail.com>
2021-03-17 00:59:26 +0200

    progresss

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 console.log("Homework");
 
-const addButton = document.getElementById("addButton").addEventListener("click", addData)
+const addButton = document
+  .getElementById("addButton")
+  .addEventListener("click", addData);
+
 function addData() {
+  const taskName = document.getElementById("inputbox").value;
+  const payload = {
+    id: "idaroczi",
+    todo: [
+      {
+        checked: true,
+        item: taskName,
+      },
+      {
+        checked: false,
+        item: taskName,
+      },
+    ],
+  };
+
   fetch("https://simple-json-server-scit.herokuapp.com/todo", {
     method: "POST",
     headers: {
@@ -9,17 +27,66 @@ function addData() {
     },
     body: JSON.stringify(payload),
   }).then(getData);
+
+  const task = document.createElement("div");
+  const checkedBox = document.createElement("input");
+  const taskNameTag = document.createElement("p");
+  const removeButton = document.createElement("button");
+  const inputField = document.getElementById("inputField");
+  
+  checkedBox.type = "checkbox";
+
+  taskNameTag.innerHTML = taskName;
+  task.className = "task";
+  checkedBox.className = "checkBoxClass";
+  taskNameTag.className = "taskNameClass";
+  
+  // removeButton.className = "removeButtonTag";
+  removeButton.setAttribute("id", "removeButtonTag");
+
+  //console.log(inputFieldValue);
+  document.body.appendChild(task);
+  task.appendChild(checkedBox);
+  task.appendChild(taskNameTag);
+  task.appendChild(removeButton);
+  document.body.insertBefore(task, inputField);
+  const removeButtonAction = document
+    .getElementById("removeButtonTag")
+    .addEventListener("click", updateData);
+  function apad() {
+    let a = 4;
+    b = 5;
+
+    return a + b;
+  }
+  //console.log(removeButtonAction);
+
+
 }
 
 function getData() {
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/${idaroczi}`)
+  fetch(`https://simple-json-server-scit.herokuapp.com/todo/idaroczi`)
     .then((response) => response.json())
     .then(renderToDoList)
     .catch(() => {});
 }
 
 function updateData() {
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/${idaroczi}`, {
+  const taskName = document.getElementById("inputbox").value;
+  const payload = {
+    id: "idaroczi",
+    todo: [
+      {
+        checked: true,
+        item: taskName,
+      },
+      {
+        checked: false,
+        item: taskName,
+      },
+    ],
+  };
+  fetch(`https://simple-json-server-scit.herokuapp.com/todo/idaroczi`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -28,31 +95,8 @@ function updateData() {
   }).then(getData);
 }
 
-const payload = {
-    id: "idaroczi",
-    todo:[
-      {
-        checked: true,
-        item: "task1"
-      },
-     {
-        checked: false,
-        item: "task2"
-      },
-   ]
-}
+
 
 function renderToDoList() {
-
-    
-
-
-
-console.log("csa");
-
+  console.log("csa");
 }
-const checkedBox = document.createElement("input");
-const inputField = document.getElementById('inputField');
-    checkedBox.type = "checkbox";
-    
-    document.body.insertBefore(checkedBox, inputField );
