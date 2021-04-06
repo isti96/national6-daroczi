58b1618b90f0d017187731cb44d5e89007960ee1
Johann Cruyff <isti.daroczi@gmail.com>
2021-04-06 19:20:51 +0300

    ata

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("password");
   window.location = "/";
 });
+
+
+getData();
+
+function getData() {
+    
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(handleFetchResponse)
+    .then(useJSONResponse);
+     
+}
+
+function handleFetchResponse(response) {
+    console.log("response", response);
+    return response.json();
+}
+
+function useJSONResponse(json) {
+ console.log(json);
+ renderDogList(json);
+}
+
+
+function renderDogList(ata) {
+
+    const breeds = document.getElementById("breeds");
+    const breedsList = [];
+    const list = document.createElement("list");
+    
+    
+    console.log(ata.message);
+    breeds.innerHTML = Object.keys(ata.message);
+    breedsList.push(ata.message);
+    console.log(breedsList);
+    
+
+}
