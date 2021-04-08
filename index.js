74ece01e3e3bf349eab0757a20a15063051aacf8
Johann Cruyff <isti.daroczi@gmail.com>
2021-04-08 23:53:13 +0300

    loading

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 console.log("JavaScript - Dogs App");
 
+const breedImage = document.getElementById("breed-image");
+const pageNumber = document.getElementById("page-number");
+let j = 1;
+let dogImage = [];
+let breedsList;
+
 if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
   window.location = "/login.html";
 }
@@ -10,39 +16,80 @@ document.getElementById("logout").addEventListener("click", () => {
   window.location = "/";
 });
 
-
 getData();
 
 function getData() {
-    
-    fetch("https://dog.ceo/api/breeds/list/all")
+  fetch("https://dog.ceo/api/breeds/list/all")
     .then(handleFetchResponse)
-    .then(useJSONResponse);
-     
+    .then(useJSONResponse)
+    .then(atuka);
 }
 
 function handleFetchResponse(response) {
-    console.log("response", response);
-    return response.json();
+  return response.json();
 }
 
 function useJSONResponse(json) {
- console.log(json);
- renderDogList(json);
+  renderDogList(json);
 }
 
-
 function renderDogList(ata) {
+  const breeds = document.getElementById("breeds");
+  breedsList = Object.keys(ata.message);
 
-    const breeds = document.getElementById("breeds");
-    const breedsList = [];
-    const list = document.createElement("list");
-    
-    
-    console.log(ata.message);
-    breeds.innerHTML = Object.keys(ata.message);
-    breedsList.push(ata.message);
-    console.log(breedsList);
-    
+  for (let i = 0; i < breedsList.length; i++) {
+    let htmlElement = document.createElement("p");
+    htmlElement.innerText = breedsList[i];
+    breeds.appendChild(htmlElement);
+  }
+}
 
+function atuka() {
+  let anett = document.getElementById("breeds").childNodes;
+
+  for (let i = 0; i < anett.length; i++) {
+    anett[i].addEventListener("click", function () {
+      j = 1;
+
+      pageNumber.innerText = j;
+      let current = document.getElementsByClassName("active");
+
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+
+      this.className += " active";
+
+      fetch(`https://dog.ceo/api/breed/${anett[i].innerText}/images`)
+        .then(handleFetchResponse)
+        .then(useJSONResponseAnett);
+    });
+  }
+}
+function useJSONResponseAnett(jsonAnett) {
+  renderDogImages(jsonAnett);
+}
+
+function renderDogImages(dogs) {
+  dogImage = dogs.message;
+  breedImage.setAttribute("src", dogImage[0]);
+  document.getElementById("forward").addEventListener("click", kutya);
+}
+
+function kutya() {
+  if (j < dogImage.length) {
+    breedImage.setAttribute("src", dogImage[j]);
+    pageNumber.innerText = j + 1;
+    j++;
+  }
+}
+
+document.getElementById("backward").addEventListener("click", antikutya);
+
+function antikutya() {
+  if (j > 1) {
+    j--;
+    breedImage.setAttribute("src", dogImage[j - 1]);
+    pageNumber.innerText = j;
+  }
 }
