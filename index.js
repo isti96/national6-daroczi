0658b011256dc2e3c005bee080911fd60328b54b
Johann Cruyff <isti.daroczi@gmail.com>
2021-04-10 16:21:59 +0300

    almost

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ console.log("JavaScript - Dogs App");
 
 const breedImage = document.getElementById("breed-image");
 const pageNumber = document.getElementById("page-number");
-let j = 1;
+let breedsListHtml = document.getElementById("breeds").childNodes;
+let current = document.getElementsByClassName("active");
+let j = 0;
 let dogImage = [];
 let breedsList;
 
@@ -22,7 +24,7 @@ function getData() {
   fetch("https://dog.ceo/api/breeds/list/all")
     .then(handleFetchResponse)
     .then(useJSONResponse)
-    .then(atuka);
+    .then(renderDogImages);
 }
 
 function handleFetchResponse(response) {
@@ -33,63 +35,96 @@ function useJSONResponse(json) {
   renderDogList(json);
 }
 
-function renderDogList(ata) {
+function renderDogList(respRender) {
   const breeds = document.getElementById("breeds");
-  breedsList = Object.keys(ata.message);
+  breedsList = Object.keys(respRender.message);
+  console.log(breedsList);
 
   for (let i = 0; i < breedsList.length; i++) {
     let htmlElement = document.createElement("p");
     htmlElement.innerText = breedsList[i];
     breeds.appendChild(htmlElement);
+
+    if (
+      localStorage.getItem("index") &&
+      localStorage.getItem("breed") &&
+      breedsList[i] === localStorage.getItem("breed")
+    ) {
+      htmlElement.className = " active";
+
+      fetch(`https://dog.ceo/api/breed/${localStorage.getItem("breed")}/images`)
+        .then(handleFetchResponse)
+        .then(useJSONResponseLS);
+    }
   }
 }
 
-function atuka() {
-  let anett = document.getElementById("breeds").childNodes;
+function renderDogImages() {
+  for (let i = 0; i < breedsListHtml.length; i++) {
+    breedsListHtml[i].addEventListener("click", function () {
+      j = 0;
 
-  for (let i = 0; i < anett.length; i++) {
-    anett[i].addEventListener("click", function () {
-      j = 1;
-
-      pageNumber.innerText = j;
-      let current = document.getElementsByClassName("active");
+      pageNumber.innerText = j + 1;
 
       if (current.length > 0) {
         current[0].className = current[0].className.replace(" active", "");
       }
-
       this.className += " active";
+      console.log(current[0].innerText);
 
-      fetch(`https://dog.ceo/api/breed/${anett[i].innerText}/images`)
+      fetch(`https://dog.ceo/api/breed/${breedsListHtml[i].innerText}/images`)
         .then(handleFetchResponse)
-        .then(useJSONResponseAnett);
+        .then(useJSONResponseImages);
+      updateLocalStorage();
     });
   }
 }
-function useJSONResponseAnett(jsonAnett) {
-  renderDogImages(jsonAnett);
+function useJSONResponseImages(jsonImages) {
+  assignImages(jsonImages);
 }
 
-function renderDogImages(dogs) {
+function assignImages(dogs) {
   dogImage = dogs.message;
-  breedImage.setAttribute("src", dogImage[0]);
-  document.getElementById("forward").addEventListener("click", kutya);
+
+  breedImage.setAttribute("src", dogImage[j]);
 }
 
-function kutya() {
-  if (j < dogImage.length) {
+document.getElementById("forward").addEventListener("click", imageForward);
+
+function imageForward() {
+  if (j < dogImage.length - 1) {
+    j++;
     breedImage.setAttribute("src", dogImage[j]);
     pageNumber.innerText = j + 1;
-    j++;
+    updateLocalStorage();
   }
 }
 
-document.getElementById("backward").addEventListener("click", antikutya);
+document.getElementById("backward").addEventListener("click", imageBackward);
 
-function antikutya() {
-  if (j > 1) {
-    j--;
+function imageBackward() {
+  if (pageNumber.innerText > 1) {
+    
     breedImage.setAttribute("src", dogImage[j - 1]);
     pageNumber.innerText = j;
+    j--;
+    updateLocalStorage();
   }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("index", j);
+  localStorage.setItem("breed", current[0].innerText);
+}
+
+function useJSONResponseLS(jsonImages) {
+  assignImages2(jsonImages);
+}
+
+function assignImages2(dogs) {
+  dogImage = dogs.message;
+  j = localStorage.getItem("index");
+  breedImage.setAttribute("src", dogImage[j]);
+  pageNumber.innerText = Number(localStorage.getItem("index")) + 1;
+  
 }
