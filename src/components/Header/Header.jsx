00dc438d58ab92eeef75b3230877a80934a39825
Johann Cruyff <isti.daroczi@gmail.com>
2021-04-18 18:37:50 +0300

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import "./Header.css";
 import { AppLogo } from "../AppLogo/AppLogo";
 export function Header() {
-    return (<div className="app-header">
-        <AppLogo />
-            </div>);
+  return (
+    <div className="app-header">
+      <AppLogo />
+      <p className="app-header__title">To Do App</p>
+    </div>
+  );
 }
